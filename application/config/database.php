20d590f66342de9d10ef070f1e7a769afe5365bf
Sahil Sharma <sahilsharma011@gmail.com>
2016-01-03 21:27:22 +0530

    Fixed db creds for main server

--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'snudevelopers@123';
+$db['default']['password'] = 'HALFpast12';
 $db['default']['database'] = 'quiz';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
